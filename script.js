a4d54f33d70f0a737431915f2ee205b5d8baade9
Reena Thomas <rtthomas@ucsc.edu>
2023-07-05 17:57:41 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Get the necessary elements from the HTML document
+const nextClueButton = document.getElementById("next-clue-button");
+
+// Add an event listener to the Next Clue button
+nextClueButton.addEventListener("click", proceedToNextClue);
+
+// Function to proceed to the next clue
+function proceedToNextClue() {
+  // Add code to navigate to the next clue page
+  // You can use JavaScript to redirect the user to the next clue page
+  // For example: window.location.href = "clue2.html";
+  console.log("Proceeding to the next clue...");
+}
